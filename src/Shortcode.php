942d7083c4166adf294df09d0e6d8a8bc69630cc
Austin Passy <austin@passy.co>
2017-05-25 13:31:56 -0700

    Updated shortcode class with new attributes and format.

--- src/Shortcode.php
+++ src/Shortcode.php
@@ -7,85 +7,100 @@ namespace FrostyMedia\MSFeaturedImage;
  *
  * @package FrostyMedia\MSFeaturedImage
  */
-class Shortcode {
+class Shortcode implements WpHooksInterface {
+
+    const IGNORE_BLOG_ID = 'ignore-blog-id';
+    const IGNORE_BLOG_IDS = 'ignore-blog-ids';
+    const SHORTCODE_SLUG = 'multisite-featured-image';
 
     /**
-     * Instance of this class.
-     *
-     * @var object
+     * Add class hooks.
      */
-    protected static $instance = null;
-
-    /**
-     * @return Shortcode|object
-     */
-    public static function instance() {
-        if ( self::$instance === null ) {
-            self::$instance = new self;
-        }
-
-        return self::$instance;
+    public function addHooks() {
+        add_action( 'init', [ $this, 'addShortcode' ] );
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueueScripts' ] );
     }
 
-    public static function addShortcode() {
-        add_shortcode( 'multisite-featured-image', [ self::instance(), 'allSitesShortcode' ] );
+    /**
+     * Add the shortcode.
+     */
+    public function addShortcode() {
+        add_shortcode( self::SHORTCODE_SLUG, [ $this, 'allSitesShortcode' ] );
+    }
+
+    /**
+     * Register shortcode styles.
+     */
+    public function enqueueScripts() {
+        wp_register_style(
+            FeaturedImage::PLUGIN_SLUG,
+            plugins_url( 'assets/css/sites.css', dirname( __FILE__ ) ),
+            [],
+            FeaturedImage::VERSION
+        );
     }
 
     /**
      * Get all sites in the network
      *
-     * @ref http://wordpress.org/support/topic/how-to-create-a-list-of-all-multi-sites-or-blogs-within-one-site?replies=18
+     * @link http://wordpress.org/support/topic/how-to-create-a-list-of-all-multi-sites-or-blogs-within-one-site?replies=18
      *
      * @return string
      */
     public function allSitesShortcode( $atts ) {
-        $site_id = defined( 'SITE_ID_CURRENT_SITE' ) ? SITE_ID_CURRENT_SITE : null;
-        $atts    = shortcode_atts( [
-            'ignore-blog-id' => $site_id,
-        ], $atts, 'multisite-featured-image' );
+        $site_id = defined( 'SITE_ID_CURRENT_SITE' ) && is_numeric( SITE_ID_CURRENT_SITE ) ?
+            SITE_ID_CURRENT_SITE : null;
 
-        wp_enqueue_style( 'ms-featured-image' );
+        $atts = shortcode_atts( [
+            self::IGNORE_BLOG_ID => $site_id,
+            self::IGNORE_BLOG_IDS => [],
+        ], $atts, self::SHORTCODE_SLUG );
+
+        wp_enqueue_style( FeaturedImage::PLUGIN_SLUG );
 
         ob_start();
-        FeaturedImage::views( 'shortcode.php', $atts );
+        include dirname( __DIR__ ) . '/views/shortcode.php';
 
         return ob_get_clean();
     }
 
     /**
-     * Query all blogs from the multisite install
+     * Get all blogs in the network
      *
-     * @param int|null $ignore_blog_id The blog ID to ignore.
+     * @param array $ignore_blog_ids The blog ID's to ignore.
      *
-     * @return array|null|object
+     * @return array
      */
-    public static function getAllBlogs( $ignore_blog_id = null ) {
+    public function getAllBlogs( array $ignore_blog_ids = [] ): array {
         global $wpdb;
 
-        $where = ! empty( $ignore_blog_id ) ?
-            "WHERE blog_id != $ignore_blog_id AND public = 1" :
+        $where = ! empty( $ignore_blog_ids ) ?
+            "WHERE blog_id NOT IN (" . implode( ',', array_map( 'absint', $ignore_blog_ids ) ) . ") AND public = 1" :
             'WHERE public = 1';
 
-        return $wpdb->get_results(
+        $blogs = $wpdb->get_results(
             "SELECT blog_id, domain, path FROM $wpdb->blogs $where ORDER BY path"
         );
+
+        return is_array( $blogs ) ? $blogs : [];
     }
 
     /**
      * Get all blog names.
      *
-     * @param object $blog
+     * @link http://wordpress.stackexchange.com/a/5096/9065
      *
-     * @uses get_blog_prefix
-     * @ref http://wordpress.stackexchange.com/a/5096/9065
+     * @param string $blog_id
      *
-     * @return array|null|object
+     * @return array
      */
-    public static function getBlogNames( $blog ) {
+    public function getBlogNames( string $blog_id ): array {
         global $wpdb;
 
-        return $wpdb->get_results(
-            "SELECT option_value FROM {$wpdb->get_blog_prefix( $blog->blog_id )}options WHERE option_name = 'blogname'"
+        $blogs = $wpdb->get_results(
+            "SELECT option_value FROM {$wpdb->get_blog_prefix( $blog_id )}options WHERE option_name = 'blogname'"
         );
+
+        return is_array( $blogs ) ? $blogs : [];
     }
 }
