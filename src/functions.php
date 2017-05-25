88c8b9d3cfc2d575127ebce64ca1b841a5333033
Austin Passy <austin@passy.co>
2017-05-25 13:33:50 -0700

    Add namespace to the functions file.

--- src/functions.php
+++ src/functions.php
@@ -1,46 +1,32 @@
 <?php
 
-/**
- * Function to Convert stdClass Objects to Multidimensional Arrays.
- *
- * @ref	http://www.if-not-true-then-false.com/2009/php-tip-convert-stdclass-object-to-multidimensional-array-and-convert-multidimensional-array-to-stdclass-object/
- *
- * @return array
- */
-function ms_featured_image_object_to_array( $input ) {
-	\FrostyMedia\MSFeaturedImage\Includes\Common::objectToArray( $input );
-}
+namespace FrostyMedia\MSFeaturedImage;
 
 /**
- * Retrieves the attachment ID from the file URL.
- *
- * @param string $image_url
- *
- * @return string
- */
-function ms_featured_image_url_to_attachment_id( $image_url ) {
-	\FrostyMedia\MSFeaturedImage\Includes\Common::urlToAttachmentID( $image_url );
-}
-
-/**
- * Get the attachment image.
+ * Get the attachment image for a specific blog.
  *
  * @param int $blog_id
  * @param string $size Can be any custom image size registered
  *          defaults to 'thumbnail'
  *          optional: medium, large, full
  *          OR array( $size, $size )
- * @param bool $image Output image or return image URL
+ * @param bool $image Use `wp_get_attachment_image` vs `wp_get_attachment_image_src`
  *
  * @return string
  */
-function ms_featured_image_get_site_featured_image( $blog_id = '', $size = 'thumbnail', $image = true ) {
-	\FrostyMedia\MSFeaturedImage\Includes\Common::getSiteFeaturedImage( $blog_id, $size, $image );
+function get_site_featured_image( int $blog_id, $size = 'thumbnail', $image = true ): string {
+    return Common::getSiteFeaturedImage( $blog_id, $size, $image );
 }
 
 /**
- * Output the featured image
+ * Output the attachment image for a specific blog.
+ *
+ * @param int $blog_id
+ * @param string $size Can be any custom image size registered
+ *          defaults to 'thumbnail'
+ *          optional: medium, large, full
+ *          OR array( $size, $size )
  */
-function ms_featured_image_site_featured_image( $blog_id = '', $size = 'thumbnail' ) {
-	\FrostyMedia\MSFeaturedImage\Includes\Common::siteFeaturedImage( $blog_id, $size );
+function site_featured_image( int $blog_id, $size = 'thumbnail' ) {
+    Common::siteFeaturedImage( $blog_id, $size );
 }
