12fac774fed426631ff38566a7c95313dcf35454
Austin Passy <austin@passy.co>
2017-05-25 13:34:09 -0700

    Version 3.0.0

--- ms-featured-image.php
+++ ms-featured-image.php
@@ -5,16 +5,21 @@
  * Description: Adds a featured image to each site in a WordPress Multisite Network.
  * Version: 3.0.0
  * Author: Austin Passy
- * Author URI: http://austin.passy.co
+ * Author URI: https://austin.passy.co
  * Text Domain: ms-featured-image
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/Frosty-Media/ms-featured-image
  * Network: true
+ * GitHub Plugin URI: https://github.com/Frosty-Media/ms-featured-image
+ * GitHub Branch: master
+ * Requires WP: 4.6
+ * Requires PHP: 7.0.1
  */
 
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
+
+use FrostyMedia\MSFeaturedImage\FeaturedImage;
 
 require_once __DIR__ . '/src/Psr4Autoloader.php';
 
@@ -22,7 +27,9 @@ require_once __DIR__ . '/src/Psr4Autoloader.php';
     ->addNamespace( 'FrostyMedia\\MSFeaturedImage', __DIR__ . '/src' )
     ->register();
 
+define( FeaturedImage::class . '_FILE', __FILE__ );
+
 /**
  * Out of the frying pan, and into the fire.
  */
-\FrostyMedia\MSFeaturedImage\FeaturedImage::instance( __FILE__ );
+FeaturedImage::instance();
