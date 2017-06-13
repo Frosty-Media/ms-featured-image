a163a78291189c35dd8ac8755cb27e041da180eb
Austin Passy <apassy@beachbody.com>
2017-06-13 09:11:02 -0700

    Added notice when an incompatible version of PHP is used to avoid errors.

--- ms-featured-image.php
+++ ms-featured-image.php
@@ -3,7 +3,7 @@
  * Plugin Name: Multisite Featured Image
  * Plugin URI: https://frosty.media/plugins/multisite-featured-image/
  * Description: Adds a featured image to each site in a WordPress Multisite Network.
- * Version: 3.0.0
+ * Version: 3.0.1
  * Author: Austin Passy
  * Author URI: https://austin.passy.co
  * Text Domain: ms-featured-image
@@ -19,17 +19,36 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
-use FrostyMedia\MSFeaturedImage\FeaturedImage;
+if ( version_compare( phpversion(), '7.0.1', '>=' ) ) {
+    require_once __DIR__ . '/src/Psr4Autoloader.php';
 
-require_once __DIR__ . '/src/Psr4Autoloader.php';
+    ( new \FrostyMedia\MSFeaturedImage\Psr4Autoloader() )
+        ->addNamespace( 'FrostyMedia\\MSFeaturedImage', __DIR__ . '/src' )
+        ->register();
 
-( new \FrostyMedia\MSFeaturedImage\Psr4Autoloader() )
-    ->addNamespace( 'FrostyMedia\\MSFeaturedImage', __DIR__ . '/src' )
-    ->register();
+    define( \FrostyMedia\MSFeaturedImage\FeaturedImage::class . '_FILE', __FILE__ );
 
-define( FeaturedImage::class . '_FILE', __FILE__ );
+    \FrostyMedia\MSFeaturedImage\FeaturedImage::instance();
+} else {
+    if ( defined( 'WP_CLI' ) ) {
+        WP_CLI::warning( _ms_featured_image_php_version_text() );
+    } else {
+        add_action( 'network_admin_notices', '_beachbody_entities_php_version_error' );
+    }
+}
 
 /**
- * Out of the frying pan, and into the fire.
+ * Admin notice for incompatible versions of PHP.
  */
-FeaturedImage::instance();
+function _ms_featured_image_php_version_error() {
+    printf( '<div class="error"><p>%s</p></div>', _ms_featured_image_php_version_text() );
+}
+
+/**
+ * String describing the minimum PHP version.
+ *
+ * @return string
+ */
+function _ms_featured_image_php_version_text() {
+    return esc_html__( 'Multisite Featured Image plugin error: Your version of PHP is too old to run this plugin. You must be running PHP 7 or higher.', 'ms-featured-image' );
+}
