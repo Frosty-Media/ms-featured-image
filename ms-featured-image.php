818574ead8edabd79afd1b93c40121033a54f516
Austin Passy <367897+thefrosty@users.noreply.github.com>
2024-05-14 08:10:50 -0700

    Bump WordPress required version. Code cleanup. Resolve invalid function in network_admin_notices hook.

--- ms-featured-image.php
+++ ms-featured-image.php
@@ -8,47 +8,52 @@
  * Author URI: https://austin.passy.co
  * Text Domain: ms-featured-image
  * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * Network: true
  * GitHub Plugin URI: https://github.com/Frosty-Media/ms-featured-image
  * GitHub Branch: master
- * Requires WP: 4.6
+ * Requires WP: 6.0
  * Requires PHP: 7.0.1
  */
 
-// Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
+use FrostyMedia\MSFeaturedImage\FeaturedImage;
+use FrostyMedia\MSFeaturedImage\Psr4Autoloader;
 
-if ( version_compare( phpversion(), '7.0.1', '>=' ) ) {
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+if (version_compare(phpversion(), '7.0.1', '>=')) {
     require_once __DIR__ . '/src/Psr4Autoloader.php';
 
-    ( new \FrostyMedia\MSFeaturedImage\Psr4Autoloader() )
-        ->addNamespace( 'FrostyMedia\MSFeaturedImage', __DIR__ . '/src' )
-        ->register();
+    (new Psr4Autoloader())->addNamespace('FrostyMedia\MSFeaturedImage', __DIR__ . '/src')->register();
 
-    define( \FrostyMedia\MSFeaturedImage\FeaturedImage::class . '_FILE', __FILE__ );
+    define(FeaturedImage::class . '_FILE', __FILE__);
 
-    \FrostyMedia\MSFeaturedImage\FeaturedImage::instance();
+    FeaturedImage::instance();
 } else {
-    if ( defined( 'WP_CLI' ) ) {
-        WP_CLI::warning( _ms_featured_image_php_version_text() );
+    if (defined('WP_CLI') && WP_CLI && method_exists('WP_CLI', 'warning')) {
+        WP_CLI::warning(_ms_featured_image_php_version_text());
     } else {
-        add_action( 'network_admin_notices', '_beachbody_entities_php_version_error' );
+        return add_action('network_admin_notices', '_ms_featured_image_php_version_error');
     }
 }
 
 /**
  * Admin notice for incompatible versions of PHP.
  */
-function _ms_featured_image_php_version_error() {
-    printf( '<div class="error"><p>%s</p></div>', _ms_featured_image_php_version_text() );
+function _ms_featured_image_php_version_error()
+{
+    printf('<div class="error"><p>%s</p></div>', _ms_featured_image_php_version_text());
 }
 
 /**
  * String describing the minimum PHP version.
- *
  * @return string
  */
-function _ms_featured_image_php_version_text() {
-    return esc_html__( 'Multisite Featured Image plugin error: Your version of PHP is too old to run this plugin. You must be running PHP 7 or higher.', 'ms-featured-image' );
+function _ms_featured_image_php_version_text()
+{
+    return esc_html__(
+        'Multisite Featured Image plugin error: Your version of PHP is too old to run this plugin. You must be running PHP 7 or higher.',
+        'ms-featured-image'
+    );
 }
