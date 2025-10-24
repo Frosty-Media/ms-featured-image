4203c16818762f9dcaed0f989ec7b33d8d05d4a2
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-10-24 12:17:19 -0700

    Formatting and code cleanup.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,76 +1,70 @@
-( function ($) {
-
-  "use strict";
+(function ($) {
+  'use strict'
 
   // WP 3.5+ uploader
   $(function () {
-    var file_frame;
-    window.formfield = '';
+    let file_frame
+    window.formfield = ''
 
     $(document.body).on('click', 'input[type="button"].button.wpsf-browse', function (e) {
 
-      e.preventDefault();
+      e.preventDefault()
 
-      var button = $(this);
+      const button = $(this)
 
-      window.formfield = $(this).closest('td');
+      window.formfield = $(this).closest('td')
 
       // If the media frame already exists, reopen it.
       if (file_frame) {
-        file_frame.open();
-        return;
+        file_frame.open()
+        return
       }
 
       // Create the media frame.
       file_frame = wp.media.frames.file_frame = wp.media({
-        frame: 'post',
-        state: 'insert',
-        title: button.data('uploader_title'),
-        button: {
+        frame: 'post', state: 'insert', title: button.data('uploader_title'), button: {
           text: button.data('uploader_button_text')
-        },
-        library: {
+        }, library: {
           type: 'image'
-        },
-        multiple: false  // Set to true to allow multiple files to be selected
-      });
+        }, multiple: false  // Set to true to allow multiple files to be selected
+      })
 
       file_frame.on('menu:render:default', function (view) {
         // Store our views in an object.
-        var views = {};
+        const views = {}
 
         // Unset default menu items
-        view.unset('library-separator');
-        view.unset('gallery');
-        view.unset('featured-image');
-        view.unset('embed');
+        view.unset('library-separator')
+        view.unset('gallery')
+        view.unset('featured-image')
+        view.unset('embed')
 
         // Initialize the views in our view object.
-        view.set(views);
-      });
+        view.set(views)
+      })
 
       // When an image is selected, run a callback.
       file_frame.on('insert', function () {
-
-        var attachment = file_frame.state().get('selection').first().toJSON();
-        window.formfield.find('input[type="text"]').val(attachment.url);
-      });
+        const attachment = file_frame.state().get('selection').first().toJSON()
+        window.formfield.find('input[type="text"]').val(attachment.url)
+        window.formfield.find('input[type="hidden"]').val(attachment.id)
+      })
 
       // Finally, open the modal
-      file_frame.open();
-    });
+      file_frame.open()
+    })
 
     // WP 3.5+ uploader
-    window.formfield = '';
+    window.formfield = ''
 
     $('input[type="button"].button.wpsf-clear').on('click', function (e) {
-      e.preventDefault();
-      $(this).closest('td').find('input[type="text"]').val('');
-    });
+      e.preventDefault()
+      $(this).closest('td').find('input[type="text"]').val('')
+    })
 
-    // Wraoper fix 3.8
+    // Wrapper fix 3.8
     $('div#ms_featured_image form > table.form-table').wrap('<div class="inside"/>')
 
-  });//function()
+  })//function()
 
-}(jQuery));
+}(jQuery))
